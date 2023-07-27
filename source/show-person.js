9172f6fd1d03c04f5ffec3be5c67006fd8e92d7b
hjklog <hjklog98@likelion.org>
2023-07-27 16:22:15 +0900

    만든이들 반응형 수정

--- source/show-person.js
+++ source/show-person.js
@@ -6,13 +6,13 @@ fetch('source/person.json').then(function(response){
 }).then(function(items){
     items.forEach(function(items, i){
         const template = `
-            <div class="col-6 col-md-2 p-2">
+            <div class="col-6 col-md-4 col-lg-2 p-2">
                 <div class="profile-card text-center py-4">
-                    <img src="${items.image}" alt="프로필 이미지">
-                    <div class="pretendard-font fw-medium fs-4">${items.name}</div>
-                    <div class="pretendard-font fw-medium">${items.department}</div>
-                    <div class="pretendard-font fw-medium mt-3">${items.position}</div>
-                    <div class="pretendard-font fw-medium">${items.role}</div>
+                    <img class="img-fluid" src="${items.image}" alt="프로필 이미지">
+                    <div class="pretendard-font maker-name">${items.name}</div>
+                    <div class="pretendard-font maker-department">${items.department}</div>
+                    <div class="pretendard-font maker-position mt-3">${items.position}</div>
+                    <div class="pretendard-font maker-role">${items.role}</div>
                 </div>
             </div>
         `;
