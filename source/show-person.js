24e92d1f5b6148c52b3309b4d95f6078072ab916
hjklog <hjklog98@likelion.org>
2023-07-28 01:59:30 +0900

    만든이들 카드 배경

--- source/show-person.js
+++ source/show-person.js
@@ -7,12 +7,12 @@ fetch('source/person.json').then(function(response){
     items.forEach(function(items, i){
         const template = `
             <div class="col-6 col-md-4 col-lg-2 p-2">
-                <div class="profile-card text-center py-4">
+                <div class="profile-card text-center py-4 ${items.class}">
                     <img class="img-fluid" src="${items.image}" alt="프로필 이미지">
-                    <div class="pretendard-font maker-name">${items.name}</div>
-                    <div class="pretendard-font maker-department">${items.department}</div>
-                    <div class="pretendard-font maker-position mt-3">${items.position}</div>
-                    <div class="pretendard-font maker-role">${items.role}</div>
+                    <div class="pretendard-font maker-name text-white">${items.name}</div>
+                    <div class="pretendard-font maker-department text-white">${items.department}</div>
+                    <div class="pretendard-font maker-position mt-3 text-white">${items.position}</div>
+                    <div class="pretendard-font maker-role text-white">${items.role}</div>
                 </div>
             </div>
         `;
