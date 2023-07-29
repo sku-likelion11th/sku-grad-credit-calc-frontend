c257d053f83f841e806abd51b11265e69a8b841d
hjklog <hjklog98@likelion.org>
2023-07-30 03:25:14 +0900

    파일 업로드 폼 추가

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,18 @@
+// function onClickUpload() {
+//   let myInput = document.getElementById("input");
+//   myInput.click();
+// }
 function onClickUpload() {
   let myInput = document.getElementById("input");
   myInput.click();
 }
+
+document.getElementById('input').addEventListener('change', function() {
+  // 파일이 선택 되었을 때 폼을 자동으로 전송
+  document.getElementById('uploadForm').submit();
+});
+
+document.getElementById('uploadBtn').addEventListener('click', function(event) {
+  // 기본 Submit 이벤트 방지
+  event.preventDefault();
+});
